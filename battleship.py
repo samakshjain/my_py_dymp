c096f1c970919f9eb746b59de599b7ca4726d76e
samakshjain <samakshjain@live.com>
2014-08-28 16:29:13 +0530

    Update
    Added comments and cls

--- battleship.py
+++ battleship.py
@@ -1,5 +1,7 @@
+#for a random int
 from random import randint
 
+#for clearing screen
 class cls(object):
     def __repr__(self):
         import os
@@ -8,35 +10,41 @@ class cls(object):
 
 cls = cls()
 
+#Welcome text + prompt
 print "BATTLESHIP!"
 turns = prompt("How many turns would you like to take?")
-board = []
+board_size = prompt("What should be the board size?")
+cls
 
-for x in range(5):
-    board.append(["O"] * 5)
+#initializing board to all O's
+board = [["O" for x in range(board_size)][for y in range(board_size)]]
+
 
 def print_board(board):
+    #function to display board clearly
     for row in board:
         print " ".join(row)
 
-print "Let's play Battleship!"
+#display initial state
 print_board(board)
 
+#placing a ship
 def random_row(board):
     return randint(0, len(board) - 1)
 
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
+#debug
 #ship_row = random_row(board)
 #ship_col = random_col(board)
 print ship_row
 print ship_col
 
-# Everything from here on should go in your for loop!
-# Be sure to indent four spaces!
-
+#simple if else cases
 for turn in range(turns):
+    cls
+    print  "Turn #" + str(turn+1) 
     guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Col:"))
     if guess_row == ship_row and guess_col == ship_col:
@@ -47,12 +55,13 @@ for turn in range(turns):
             print "Oops, that's not even in the ocean."
         elif(board[guess_row][guess_col] == "X"):
             print "You guessed that one already."
+            
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
-        if turn == 3 :
+        if turn == turns-1 :
             print "Game Over"
             board[ship_row][ship_col] = "S"
+            print "Dagnabbit! The ship was hiding here >"
             print_board(board)
-        print turn+1
         print_board(board)
