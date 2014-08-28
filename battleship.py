74e49859b141d01ff891fbec862f12a80fd2983b
samakshjain <samakshjain@live.com>
2014-08-28 19:27:12 +0530

    Updated to py34
    2to3 and QoL changes.

--- battleship.py
+++ battleship.py
@@ -11,19 +11,23 @@ class cls(object):
 cls = cls()
 
 #Welcome text + prompt
-print "BATTLESHIP!"
-turns = prompt("How many turns would you like to take?")
-board_size = prompt("What should be the board size?")
+print ("\t~~~~~////////BATTLESHIP!////////~~~~~\n")
+turns = eval(input("How many turns would you like to take?\n"))
+board_size = eval(input("What should be the board size?\n"))
 cls
 
 #initializing board to all O's
-board = [["O" for x in range(board_size)][for y in range(board_size)]]
+#board = [["O" for x in range(board_size)][for y in range(board_size)]]
+board =[]
+for x in range(5):
+    board.append(["O"] * 5)
+
 
 
 def print_board(board):
     #function to display board clearly
     for row in board:
-        print " ".join(row)
+        print((" ".join(row)))
 
 #display initial state
 print_board(board)
@@ -35,33 +39,33 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
+
+ship_row = random_row(board)
+ship_col = random_col(board)
 #debug
-#ship_row = random_row(board)
-#ship_col = random_col(board)
-print ship_row
-print ship_col
+print (ship_row)
+print (ship_col)
 
 #simple if else cases
 for turn in range(turns):
     cls
-    print  "Turn #" + str(turn+1) 
-    guess_row = int(raw_input("Guess Row:"))
-    guess_col = int(raw_input("Guess Col:"))
+    print(("Turn #" + str(turn+1)))
+    guess_row = int(eval(input("\n\n\tGuess Row:   ")))
+    guess_col = int(eval(input("\n\n\tGuess Col:   ")))
     if guess_row == ship_row and guess_col == ship_col:
-        print "Congratulations! You sunk my battleship!"
+        print ("\n\n\tCongratulations! You sunk my battleship!")
         break
     elif turn <turns:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print "Oops, that's not even in the ocean."
+        if (guess_row < 0 or guess_row > board_size) or (guess_col < 0 or guess_col > board_size):
+            print ("\n\n\tOops, that's not even in the ocean.")
         elif(board[guess_row][guess_col] == "X"):
-            print "You guessed that one already."
+            print ("\n\n\tYou guessed that one already.")
             
         else:
-            print "You missed my battleship!"
+            print ("\n\n\tYou missed my battleship!")
             board[guess_row][guess_col] = "X"
         if turn == turns-1 :
-            print "Game Over"
+            print ("\n\n\tGame Over")
             board[ship_row][ship_col] = "S"
-            print "Dagnabbit! The ship was hiding here >"
-            print_board(board)
+            print ("\n\n\tDagnabbit! The ship was hiding here >")
         print_board(board)
