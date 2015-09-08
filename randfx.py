9000523c14b2d39203382802ec0409b8c9ee917f
Samaksh Jain <samakshjain@live.com>
2015-09-08 14:58:11 +0530

    update scrabble_score

--- randfx.py
+++ randfx.py
@@ -39,11 +39,12 @@ score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
          "x": 8, "z": 10}
 def scrabble_score(word) :
-    word = word.lower()
-    total = 0
-    for letter in word :
-        if letter in score :
-            total+=score[letter]
-    else : 
-        print "bad input"
-    return total
+    try:
+        word = word.lower()
+        total = 0
+        for letter in word :
+            if letter in score :
+                total+=score[letter]
+        return total
+    except:
+    	print "Something's not write with your 'word'"
